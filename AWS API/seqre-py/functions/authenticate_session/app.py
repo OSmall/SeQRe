d0a131ec0ac8f7e75b638e9a69d1c62d65076d63
theriddler <riddle.hunter@gmail.com>
2021-07-30 16:20:14 +1000

    removed default comments

--- AWS API/seqre-py/functions/authenticate_session/app.py
+++ AWS API/seqre-py/functions/authenticate_session/app.py
@@ -6,27 +6,6 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import SHA256
 
-"""Sample pure Lambda function
-
-Parameters
-----------
-event: dict, required
-    API Gateway Lambda Proxy Input Format
-
-    Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-context: object, required
-    Lambda Context runtime methods and attributes
-
-    Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-Returns
-------
-API Gateway Lambda Proxy Output Format: dict
-
-    Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-"""
-
 dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
