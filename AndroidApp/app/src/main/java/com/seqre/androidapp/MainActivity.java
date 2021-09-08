22aa62eedca31335a3d0833299587bacc1158f7b
Oliver Small <oliver.small232@gmail.com>
2021-09-08 13:58:21 +1000

    KeyStorage

--- AndroidApp/app/src/main/java/com/seqre/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/seqre/androidapp/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        KeyStorage keyStorage = new KeyStorage();
     }
 
 }
