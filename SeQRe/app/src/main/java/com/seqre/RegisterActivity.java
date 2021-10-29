73c99e2971fffe2261e1188cfa0f5ed3c143d498
theriddler <riddle.hunter@gmail.com>
2021-10-29 11:25:11 +1100

    added register page

--- SeQRe/app/src/main/java/com/seqre/RegisterActivity.java
+++ SeQRe/app/src/main/java/com/seqre/RegisterActivity.java
@@ -3,16 +3,21 @@ package com.seqre;
 import android.os.Bundle;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import android.widget.ImageView;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     TextView register_txt;
+    ImageView logo_register;
+    EditText email, userID, password;
 
 
     @Override
@@ -22,6 +27,15 @@ public class RegisterActivity extends AppCompatActivity {
 
         //create local variables
         register_txt = findViewById(R.id.registerText);
+
+        //setup logo
+        logo_register = findViewById(R.id.logo_register);
+        logo_register.setImageResource(R.drawable.big_logo);
+
+        //setup input texts
+        email = findViewById(R.id.email);
+        userID = findViewById(R.id.userID);
+        password = findViewById(R.id.password);
     }
 
     public void register() {
