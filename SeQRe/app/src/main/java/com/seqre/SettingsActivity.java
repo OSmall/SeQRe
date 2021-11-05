0b6044195db32ea89cd3d3b26123d82f543c3b5e
theriddler <riddle.hunter@gmail.com>
2021-11-05 16:42:11 +1100

    android updates

--- SeQRe/app/src/main/java/com/seqre/SettingsActivity.java
+++ SeQRe/app/src/main/java/com/seqre/SettingsActivity.java
@@ -1,13 +1,20 @@
 package com.seqre;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.RadioButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SettingsActivity extends AppCompatActivity {
 
     TextView settings_txt;
+    ImageView logo_settings;
+    EditText set_email, set_userID, set_password;
+    RadioButton biometric_yes, biometric_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,35 @@ public class SettingsActivity extends AppCompatActivity {
 
         //create local variables
         settings_txt = findViewById(R.id.settingsText);
+
+        //setup logo
+        logo_settings = findViewById(R.id.logo_register);
+        logo_settings.setImageResource(R.drawable.big_logo);
+
+        //setup radiogroup
+        biometric_yes = findViewById(R.id.biometric_yes);
+        biometric_no = findViewById(R.id.biometric_no);
+
+        //setup input texts
+        set_email = findViewById(R.id.email);
+        set_userID = findViewById(R.id.userID);
+        set_password = findViewById(R.id.password);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.biometric_yes:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.biometric_no:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
